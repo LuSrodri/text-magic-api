c39993b093dc1d5beed92f65b162b551745155d4
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-12 10:52:28 -0300

    Defining the /explain-code endpoint

--- app.js
+++ app.js
@@ -12,19 +12,13 @@ const openai = new OpenAIApi(configuration);
 
 app.post('/text-to-emoji', async (req, res) => {
     let input;
-    if (req.body) {
-        if (req.body.input) {
-            input = req.body.input;
-        }
-        else {
-            res.sendStatus(400);
-            return;
-        }
-    }
-    else {
+
+    if(!verifyIfHasInputBody(req)) {
         res.sendStatus(400);
         return;
     }
+
+    input = req.body.input;
 
     const isProfane = await openai.createModeration({ input: input });
 
@@ -52,6 +46,57 @@ app.post('/text-to-emoji', async (req, res) => {
     }
 
 });
+
+app.post('/explain-code', async (req, res) => {
+    let input;
+
+    if(!verifyIfHasInputBody(req)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    input = req.body.input;
+
+    const isProfane = await openai.createModeration({ input: input });
+
+    if (isProfane.data.results[0].flagged) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const response = await openai.createCompletion({
+            model: "code-davinci-002",
+            prompt: ("Explain the code below:\n" + input),
+            temperature: 0.1,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        res.statusCode = 200;
+        res.send({ output: response.data.choices[0].text });
+    }
+    catch {
+        res.sendStatus(400);
+    }
+
+});
+
+function verifyIfHasInputBody(req) {
+    if (req.body) {
+        if (req.body.input) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
 
 
 app.listen(PORT, () => {
