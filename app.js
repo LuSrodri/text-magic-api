1b19be089a0472c2561df49c3dbae0f4e0448e87
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-11 21:10:42 -0300

    Initialize express.js and adding a .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+var express = require('express');
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(PORT, function () {
+    console.log('App listening on port 3000!');
+    }
+);
