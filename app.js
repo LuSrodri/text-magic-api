8ee1ad70069e2db853a01358245826c63a508e1d
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-02-20 16:23:13 -0300

    Adding a /html-to-markdown, /markdown-to-html, /js-to-python and /python-to-js endpoints

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.post('/explain-code', async (req, res) => {
             frequency_penalty: 0,
             presence_penalty: 0,
             user: uuidv4(),
-          });
+        });
 
         res.statusCode = 200;
         res.send({ output: response.data.choices[0].text });
@@ -151,6 +151,158 @@ app.post('/fixes-text', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: ("Rewrite the text below fixing the possible mistakes: \n\n Text: \"\"\" \n" + input + "\n\"\"\""),
+            temperature: 0.1,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            user: uuidv4(),
+        });
+
+        res.statusCode = 200;
+        res.send({ output: response.data.choices[0].text });
+    }
+    catch {
+        res.sendStatus(400);
+    }
+
+});
+
+app.post('/html-to-markdown', async (req, res) => {
+    let input;
+
+    if (!verifyIfHasInputBody(req)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    input = req.body.input;
+
+    const isProfane = await openai.createModeration({ input: input });
+
+    if (isProfane.data.results[0].flagged) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: ("Rewrite the html code below in markdown style: \n\n Code: \"\"\" \n" + input + "\n\"\"\""),
+            temperature: 0.1,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            user: uuidv4(),
+        });
+
+        res.statusCode = 200;
+        res.send({ output: response.data.choices[0].text });
+    }
+    catch {
+        res.sendStatus(400);
+    }
+
+});
+
+app.post('/markdown-to-html', async (req, res) => {
+    let input;
+
+    if (!verifyIfHasInputBody(req)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    input = req.body.input;
+
+    const isProfane = await openai.createModeration({ input: input });
+
+    if (isProfane.data.results[0].flagged) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: ("Rewrite the markdown code below in html: \n\n Code: \"\"\" \n" + input + "\n\"\"\""),
+            temperature: 0.1,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            user: uuidv4(),
+        });
+
+        res.statusCode = 200;
+        res.send({ output: response.data.choices[0].text });
+    }
+    catch {
+        res.sendStatus(400);
+    }
+
+});
+
+app.post('/js-to-python', async (req, res) => {
+    let input;
+
+    if (!verifyIfHasInputBody(req)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    input = req.body.input;
+
+    const isProfane = await openai.createModeration({ input: input });
+
+    if (isProfane.data.results[0].flagged) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: ("Rewrite the javascript code below in useful python code: \n\n Code: \"\"\" \n" + input + "\n\"\"\""),
+            temperature: 0.1,
+            max_tokens: 64,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            user: uuidv4(),
+        });
+
+        res.statusCode = 200;
+        res.send({ output: response.data.choices[0].text });
+    }
+    catch {
+        res.sendStatus(400);
+    }
+
+});
+
+app.post('/python-to-js', async (req, res) => {
+    let input;
+
+    if (!verifyIfHasInputBody(req)) {
+        res.sendStatus(400);
+        return;
+    }
+
+    input = req.body.input;
+
+    const isProfane = await openai.createModeration({ input: input });
+
+    if (isProfane.data.results[0].flagged) {
+        res.sendStatus(400);
+        return;
+    }
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: ("Rewrite the python code below in useful javascript code: \n\n Code: \"\"\" \n" + input + "\n\"\"\""),
             temperature: 0.1,
             max_tokens: 64,
             top_p: 1,
